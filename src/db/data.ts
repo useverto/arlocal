20101ad4233dab4f222c3c6780b34b24c97aadde
andreespirela <andreespirela@outlook.com>
2022-02-23 09:52:26 -0500

    stabilizing

--- src/db/data.ts
+++ src/db/data.ts
@@ -1,12 +1,9 @@
-import { join } from 'path';
 import { uploadData, getBlockData } from './gcp-storage';
 
 export class DataDB {
-  // DB should be emptied on every run.
-  private path: string;
 
+  // @ts-ignore
   constructor(dbPath: string) {
-    this.path = join(dbPath, 'data-');
   }
 
   async insert(obj: { txid: string; data: string }): Promise<{ txid: string; data: string }> {
